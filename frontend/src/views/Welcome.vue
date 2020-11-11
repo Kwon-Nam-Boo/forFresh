fc25b6b11656c221982f2ad80f84a1382e7774f6
신혜민 <tlsgpals1004@naver.com>
2020-11-11 15:41:46 +0900

    네브바 수정

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -13,7 +13,7 @@
     <v-carousel-item
       v-for="(pic, i) in pics"
       :key="i"
-      :src="pic.src"
+      :src="require(`@/assets/${pic.src}`)"
     >
       <!-- <v-sheet
         :color="color"
@@ -30,6 +30,15 @@
           </div>
         </v-row>
       </v-sheet> -->
+      <!-- <div class="move">
+      <v-btn
+          class="mt-13"
+          @click="$router.push('/').catch(() => {})" >
+          forfresh로 이동하기
+          
+          </v-btn>
+      </div> -->
+      <a  @click="$router.push('/login').catch(() => {})" class="btn">forfresh   >></a>
     </v-carousel-item>
   </v-carousel> 
 </template>
@@ -45,8 +54,8 @@ export default {
       model: 0,
       
       pics:[
-        {src:"@/assets/slide1.png"},
-        {src:"@/assets/slide2.png"},
+        {src:"slide5.png"},
+        {src:"slide4.png"},
       ]
     };
   },
@@ -63,3 +72,53 @@ export default {
   
 };
 </script>
+<style scoped>
+.move{
+  float:right;
+}
+.vtn {
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   height: 5vh;
+   width: 1em;
+   background: #7cd4d4;
+   
+}
+.btn {
+   font-size: 1em;
+   font-weight:bold;
+   font-family:Roboto;
+   border: 3px solid rgb(58, 58, 58);
+   color:green;
+   text-decoration: none;
+   border-radius: 100em;
+   padding: 10px 20px;
+   letter-spacing: 0.05em;
+   position: relative;
+   transition: 0.3s;
+   float:right;
+   margin-top: 5%;
+   
+}
+.btn::before {
+   content: '';
+   border: 3px solid #f9f6f1;
+   width: 100%;
+   height: 100%;
+   position: absolute;
+   left: 3px;
+   top: 2px;
+   border-radius: 100em;
+   background: #f9f6f1;
+   z-index: -1;
+   transition: 0.3s;
+}
+.btn:hover::before {
+   left:0;
+   top:0;
+   width: calc(100% - 4px);
+   height: calc(100% - 4px);
+}
+</style>
